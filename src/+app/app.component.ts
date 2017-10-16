d21c54bdee534a939cb897020483ebb4075a6416
susanlu <susan.lu@telus.com>
2017-10-16 13:47:57 -0700

    test

--- src/+app/app.component.ts
+++ src/+app/app.component.ts
@@ -43,7 +43,7 @@ export class XLargeDirective {
   <div class="hero-universal">
     <div class="inner-hero">
       <div>
-        <span xLarge>You Rock With DevOps, {{ title }}!</span>
+        <span xLarge>Hey, You Rock With DevOps, {{ title }}!</span>
       </div>
 
       Type here your name: <input type="text" [value]="title" (input)="title = $event.target.value">
